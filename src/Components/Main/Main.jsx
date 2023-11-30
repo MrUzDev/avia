4299fcfbf6ec24fdcaed13dd20c014f6c61830ee
Ozodbek <125816559+MrUzDev@users.noreply.github.com>
2023-11-30 13:38:11 +0500

    build

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -383,6 +383,7 @@ function Main() {
                         numberOfMonths={2}
                         portal={true}
                         range
+                        value={myAirlinesDate}
                         ref={datePickerRef}
                         style={{ display: 'none' }}
                       />
